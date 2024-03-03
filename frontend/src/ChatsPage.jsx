4160115a0f1d5e795594181d3f7a9f4ec39888c7
toshi-Devs <trahim87mustapha@gmail.com>
2024-03-03 15:26:34 +0200

    Mini-chat Done

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced';
 
-const ChatsPage = () => {
-  return (
-    <div>Chats...</div>
+const ChatsPage = (props) => {
+
+    const chatProps = useMultiChatLogic(
+
+        '65181ce0-77dd-41ea-b96b-8428149450c4',
+        props.user.username,
+        props.user.secret
+    );
+    
+    return (
+
+    <div style={{ height: '100vh' }}>
+
+        <MultiChatSocket {...chatProps} />
+        <MultiChatWindow {...chatProps} style={{ height: '100%'}}/>
+
+    </div>
+
   )
 }
 
